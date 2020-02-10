16fdb711ba2387512b29d7073af8a666a5c0877d
Karun <MercuryBromine01@gmail.com>
2020-02-10 22:23:21 +0000

    Made the program so it can do multiple months. Need to clean up code and conduct furthur testing. Also need to add the return prices function

--- init.py
+++ init.py
@@ -13,48 +13,53 @@ class flightBot:
         self.findFlights()
 
     def findFlights(self):
-        self.date = datetime.datetime.strptime("01 May 2020","%d %b %Y")
-        self.prices = []
+        userDepartureDate = input("Enter the departure date in the following format: 'Day' 'Month', 'Year'\nFor example 05 May 2020\nDeparture Date: ")
+        userArrivalDate = input("Enter the last departure date in the following format: 'Day' 'Month' 'Year'\nFor example: '06 Jun 2020'\nArrival Date: ")
+        self.date = datetime.datetime.strptime(userDepartureDate,"%d %b %Y")
+        self.arrival_date = datetime.datetime.strptime(userArrivalDate,"%d %b %Y")
+        self.delta = self.arrival_date - self.date
+        self.delta = self.delta.days + 1
+        print(self.delta)
+
         self.driver.get("https://www.qatarairways.com/en-gb/homepage.html")
 
         sleep(12)
 
         #Departure Destination#
-        #userDeparture = input("Enter the departure airport in the following format 'City' '(Airport Code)'\nFor example: 'Cardiff (CWL)'\nDeparture Airport: ")
         departure_field = self.driver.find_element_by_xpath('//*[@id="T7-from"]')
         departure_field.click()
-        departure_field.send_keys('Cardiff (CWL)') #userDeparture
+        departure_field.send_keys('Cardiff (CWL)') 
 
         sleep(1)
 
         #Arrival Destination#
 
-        #userArrival = input("Enter the arrival airport in the following format: 'City' '(Airport Code)'\nFor example: 'Cardiff (CWL)'\nArrival Airport: ")
         arrival_field = self.driver.find_element_by_xpath('//*[@id="T7-to"]')
         arrival_field.click()
-        arrival_field.send_keys('Amritsar (ATQ)') #userArrival
+        arrival_field.send_keys('Amritsar (ATQ)') 
 
         sleep(1)
 
         #Departure Date#
 
-        #userDepartureDate = input("Enter the departure date in the following format: 'Day' 'Month' 'Year'\nFor example: '06 Jun 2020'\nDeparture Date: ")
         departure_date = self.driver.find_element_by_xpath('//*[@id="T7-departure_1"]')
         departure_date.click()
         departure_date.clear()
 
-        self.tempDate = self.date.strftime('%d %b %Y')
-        departure_date.send_keys(self.tempDate)
+        tempDate = self.date.strftime('%d %b %Y')
+        departure_date.send_keys(tempDate)
 
         sleep(1)
 
         #Arrival Date#
-
-        #userArrivalDate = input("Enter the arrival date in the following format: 'Day' 'Month' 'Year'\nFor example: '06 Jun 2020'\nArrival Date: ")
         arrival_date = self.driver.find_element_by_xpath('//*[@id="T7-arrival_1"]')
         arrival_date.click()
         arrival_date.clear()
-        arrival_date.send_keys('06 Jun 2020') #userArrivalDate
+
+        arrival_temp_date = self.arrival_date
+        arrival_temp_date += datetime.timedelta(days=15)
+        arrival_temp_date = arrival_temp_date.strftime('%d %b %Y')
+        arrival_date.send_keys(arrival_temp_date) 
 
         sleep(1)
 
@@ -74,41 +79,39 @@ class flightBot:
         #Search Button#
         search_btn = self.driver.find_element_by_xpath('//*[@id="T7-search"]')
         search_btn.click()
+        sleep(25)
         self.findPrices()
 
     def findPrices(self):
-
-        sleep(15)
-
         more_dates = self.driver.find_element_by_xpath('//*[@id="flightDetailForm_outbound:calendarInitiator_OutBound"]')
         more_dates.click()
-        sleep(25)
+        sleep(35)
         content = self.driver.page_source
         soup = BeautifulSoup(content)
-        tempDate = self.date.strftime('%m-%d-%Y')
-        dateArr = tempDate.split('-')
-        dateArr[1] = list(dateArr[1])
-        dateArr[1][0] = int(dateArr[1][0])
-        dateArr[1][1] = int(dateArr[1][1])
         priceArr = []
-
-        for i in range(1,32):
-            date_id = 'OB1_'+dateArr[0]+str(dateArr[1][0])+str(dateArr[1][1])+dateArr[2]
-            price = soup.find(id=date_id)
-            if price == None:
-                next_page = self.driver.find_element_by_xpath('//*[@id="monthlyCalendarForm:j_id_22f:0:nex"]')
-                next_page.click()
-                sleep(30)
-                content = self.driver.page_source
-                soup = BeautifulSoup(content)
+        price_date = self.date
+        for i in range(1,self.delta):
+            try:
+                tmp_date = price_date.strftime('%m%d%Y')
+                date_id = 'OB1_'+tmp_date
                 price = soup.find(id=date_id)
-                priceArr.append(price.text)
-            else:
-                priceArr.append(price.text)
-            dateArr[1][1] += 1
-            if dateArr[1][1] == 10:
-                dateArr[1][0] += 1
-                dateArr[1][1] = 0
+                if price == None:
+                    next_page = self.driver.find_element_by_xpath('//*[@id="monthlyCalendarForm:j_id_22f:0:nex"]')
+                    next_page.click()
+                    sleep(35)
+                    content = self.driver.page_source
+                    soup = BeautifulSoup(content)
+                    price = soup.find(id=date_id)
+                    priceArr.append(price.text)
+                    price_date += datetime.timedelta(days=1)
+                else:
+                    priceArr.append(price.text)
+                    price_date += datetime.timedelta(days=1)
+            except Exception:
+                back = self.driver.find_element_by_xpath('//*[@id="monthlyCalendarForm:calReturnFlow"]/div[1]/button')
+                back.click()
+                sleep(2)
+                continue
 
         count = 0
         for i in priceArr:
@@ -119,16 +122,20 @@ class flightBot:
         f = open("prices.txt", "w").close()
         f = open("prices.txt", "a")
         count = 1
-        monthYearDate = self.date.strftime('%d-%b-%Y')
-        monthYearDateArr = monthYearDate.split('-')
 
-        min_date = priceArr.index(min(priceArr)) + 1
+        file_date = self.date
+        tmp_date = file_date
+        min_date = priceArr.index(min(priceArr)) #+ 1
+        tmp_date += datetime.timedelta(days=min_date)
+        tmp_date = tmp_date.strftime('%d %b %Y')
         min_price = min(priceArr)
-        f.write("The minimum price is on " + str(min_date) + " " + monthYearDateArr[1] + " " +  monthYearDateArr[2] + " " + str(min_price) + "\n")
+
+        f.write("The minimum price is on " + str(min_date) + " " +tmp_date+ " : " +  + "£" + str(min_price) + "\n")
 
         for i in priceArr:
-            f.write("The price on " + str(count) + " " + monthYearDateArr[1] + " " + monthYearDateArr[2] + ": " + str(i) + "\n")
-            count += 1
+            tmp_date = file_date.strftime('%d %b %Y')
+            f.write("The price on " + tmp_date + " : " + + "£" + str(i) + "\n")
+            file_date += datetime.timedelta(days=1)
         f.close()
 
 if __name__ == '__main__':
